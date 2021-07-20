f9d5d5fff59757f973d63a75e3b18382e666fa6d
vudang <nguyenvudang.1999@gmail.com>
2021-07-20 16:37:11 +0700

    cm1

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,34 @@ var router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{title: "Trang Chủ"});
+  res.render('index',{tab: 1, title: "Trang Chủ"});
 });
 router.get('/index', function(req, res, next) {
-  res.render('index',{title: "Trang Chủ"});
+  res.render('index',{tab: 1, title: "Trang Chủ"});
 });
 router.get('/course-list', function(req, res, next) {
-  res.render('course-list',{title: "Danh Sách Khoá Học"});
+  res.render('course-list',{tab: 3, title: "Danh Sách Khoá Học"});
 });
 router.get('/course-detail', function(req, res, next) {
   res.render('course-detail',{title: "Chi Tiết Khoá Học"});
 });
 router.get('/class-list', function(req, res, next) {
-  res.render('class-list',{title: "Danh Sách Lớp Học"});
+  res.render('class-list',{tab: 4, title: "Danh Sách Lớp Học"});
 });
 router.get('/class-detail', function(req, res, next) {
   res.render('class-detail',{title: "Chi Tiết Lớp Học"});
 });
 router.get('/support', function(req, res, next) {
-  res.render('support',{title: "Hỗ Trợ"});
+  res.render('support',{tab: 6, title: "Hỗ Trợ"});
 });
 router.get('/teachers', function(req, res, next) {
-  res.render('teachers',{title: "Danh Sách Gia Sư"});
+  res.render('teachers',{tab: 2, title: "Danh Sách Gia Sư"});
 });
 router.get('/teacher-detail', function(req, res, next) {
   res.render('teacher-detail',{title: "Thông Tin Gia Sư"});
 });
 router.get('/forums', function(req, res, next) {
-  res.render('forums',{title: "Diễn Đàn"});
+  res.render('forums',{tab: 5, title: "Diễn Đàn"});
 });
 router.get('/teacher-profile', function(req, res, next) {
   res.render('teacher-profile',{title: "Trang Cá Nhân"});
